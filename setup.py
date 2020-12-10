a13dc896c610128426376b562f1d333e59bf2dc5
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-12-10 22:55:02 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,23 +4,23 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py-altdns",
-    version="1.0.0",
-    author="Shubham Shah",
+    name="flydns",
+    version="0.1",
+    author="Pham Sy Minh",
     author_email="sshah@assetnote.io",
-    description="Generates permutations, alterations and mutations of subdomains and then resolves them.",
+    description="Generates permutations, alterations and mutations of subdomains and then resolves them",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/infosec-au/altdns",
+    url="https://github.com/shelld3v/flydns",
     packages=setuptools.find_packages(),
     entry_points={
         "console_scripts": [
-            "altdns=altdns.__main__:main",
+            "flydns=flydns.__main__:main",
         ]
     },
-    install_requires=["tldextract","argparse","termcolor","dnspython"],
+    install_requires=["tldextract","argparse","termcolor","dnspython","ipwhois"],
     classifiers=[
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.4",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Security"
