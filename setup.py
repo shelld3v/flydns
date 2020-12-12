396738eedd800cef89f2e7f4b5a455489805e2b3
shelld3v <59408894+shelld3v@users.noreply.github.com>
2020-12-12 23:25:35 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="flydns",
-    version="0.2",
+    version="0.3",
     author="Pham Sy Minh",
     author_email="phamminh0227@gmail.com",
     description="Find related subdomains from subdomains",
